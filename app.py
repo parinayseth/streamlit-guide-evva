04e517159d9112c569335698211938bd8f759a15
parinayseth <parinay.seth@gmail.com>
2024-01-19 00:41:22 +0530

    Update: Update excel datafeature

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+from datetime import datetime
+import gspread
+from google.oauth2.service_account import Credentials
 
 import os
 from langchain_community.chat_models import ChatOpenAI
@@ -20,42 +23,17 @@ load_dotenv()
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-# def get_pdf_text(pdf_docs):
-#     text = ""
-#     for pdf in pdf_docs:
-#         pdf_reader = PdfReader(pdf)
-#         for page in pdf_reader.pages:       
-#             text += page.extract_text()
-#     return text
-
-# def get_text_chunks(text):
-#     text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
-#     chunks = text_splitter.split_text(text)
-#     return chunks
-
-
-# def get_vecotr_store(text_chunks):
-#     emebeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-#     vectore_store = FAISS.from_texts(text_chunks, embedding=emebeddings)
-#     vectore_store.save_local("Training_Data")
-    
 
 
 def get_conversational_chain():
-    # prompt_template = """
-    # You are the administrator of a physician group seeking GUIDE reimbursement. Your practice, specializing in dementia care, geriatrics, and gerontology, includes dementia care specialists, geriatricians, gerontologists, and a care navigator. To strategically leverage technology and maximize your chances of success in meeting GUIDE program requirements, you're helping the organisation to fill up the form. You have been asked to provide a tailored response to the following question, ensuring alignment with GUIDE eligibility criteria, program priorities, and the specific characteristics of your organization. Keep your answer concise and impactful, within 3000 characters. Take help of the context and organisation type to answer the question.\n\n
-    # Context: {context}\n
-    # Question: {question}\n
-    # organisation_type: {organisation_type}\n
-    # """
     prompt_template = """
-    You are the administrator of a medical practitioners seeking GUIDE reimbursement. Our practice specializes in dementia care, geriatrics, and gerontology, bringing together dementia care specialists, geriatricians, gerontologists, and a care navigator. To maximize your chances of success, we're collaboratively filling out the form, leveraging technology strategically to meet GUIDE program requirements. 
-    Given the following context, please provide a tailored response to this question, ensuring alignment with GUIDE's eligibility criteria, program priorities, and our organization's specific characteristics. Keep your answer concise and impactful, within 3000 characters.
+    You are an expert in the CMS GUIDE program for dementia care. Please respond to the user's question from your available knowledge in easy to understand language. Consider user's organization type in framing your response, if applicable.\n\n
     Organization Type: {organisation_type}\n
     Context: {context} \n
     Question: {question} \n
+    Make sure the answer is properly formatted and grammatically correct. Dont give responses like - "Based on provided information" in your answers \n
     """
-    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k")
+    llm = ChatOpenAI(temperature=0, model_name="gpt-4-1106-preview", openai_api_key=os.getenv("OPENAI_API_KEY"))
 
     prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question", "organisation_type"])
     chain = load_qa_chain(llm=llm, chain_type="stuff", prompt=prompt)
@@ -68,7 +46,7 @@ def user_input(user_question,organisation_type):
     embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001", google_api_key=os.getenv("GOOGLE_API_KEY"))
     
     new_db = FAISS.load_local("Training_Data", embeddings)
-    docs = new_db.similarity_search(user_question, k=4)
+    docs = new_db.similarity_search(user_question, k=3)
 
     # llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k")
     # chain = load_qa_chain(llm=llm, chain_type="stuff")
@@ -81,10 +59,27 @@ def user_input(user_question,organisation_type):
         , return_only_outputs=True)
     print(response)
     st.write("Reply: ", response["output_text"])
+    return response["output_text"]
+
+def save_excel(organisation_name, email, organisation_type, user_question, response):
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    current_date = now.strftime("%d/%m/%Y")
+    credentials_path = "D:/may1/test app/google key/evva-health-project-a1ffe000b79e.json"
+    spreadsheet_name = 'Evva Health Guide Program'
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+    credentials = Credentials.from_service_account_file(credentials_path, scopes=scope)
+    gc = gspread.authorize(credentials)
+    spreadsheet = gc.open(spreadsheet_name)
+    worksheet = spreadsheet.get_worksheet(0)
+    row_data = [current_date, current_time, organisation_name, email, organisation_type, user_question, response]
+    worksheet.append_row(row_data)
+    
+
+    
 
 
-
-
+    
 def main():
     st.set_page_config("Welcome to CMS GUIDE Program (Evva Health)")
 
@@ -112,14 +107,15 @@ def main():
             link = "https://www.evva360.com/"  # Replace with your desired link
             st.markdown(f"""
             <a href="{link}" class="logo-img">
-                <img src="https://i.postimg.cc/tRLF6k3f/logo.jpg" width=105>
+                <img src="https://i.postimg.cc/1tdqbnQf/logo.jpg" width=105>
             </a>
             """, unsafe_allow_html=True)
 
         with col1:
             st.header("Get your answers about the CMS GUIDE Program")
-
-    organisation_type = st.text_input("Type of Organization")
+    organisation_name = st.text_input("What is the name of your organization?")
+    email = st.text_input("What is your email address?")
+    organisation_type = st.text_input("What is the type of your organization? (e.g., physician group, individual practice, health system, etc.)")
     user_question = st.text_input("Ask a Question")
 
     
@@ -129,7 +125,8 @@ def main():
         if organisation_type == "":
             st.write("Please enter a type of organization")
         with st.spinner("Processing..."):
-            user_input(user_question, organisation_type)
+            result =  user_input(user_question, organisation_type)
+            save_excel(organisation_name, email, organisation_type, user_question, result)
 
     
 
